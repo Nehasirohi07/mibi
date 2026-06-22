8147ec19b7e66cf0ce54de0213ee0c4bbd47844e
nehasirohi07 <nehasirohi665@gmail.com>
2026-06-22 17:34:09 +0530

    Added Web Search

---
+++ agents/web_search_agent.py
@@ -0,0 +1,10 @@
+def search_topic(topic):
+
+
+    resources = [
+        f"{topic} Official Documentation",
+        f"Best{topic}Tutorial",
+        f"{topic}Practice Exercises"
+    ]
+
+    return resources
