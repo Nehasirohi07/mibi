f804db9c08a98126503855f62214895208f58de5
nehasirohi07 <nehasirohi665@gmail.com>
2026-06-22 18:19:47 +0530

    add orchestrator for multi-agent course workflow

--- agents/orchestrator.py
+++ agents/orchestrator.py
@@ -1,15 +1,17 @@
 from agents.course_planner import generate_course_plan
 from agents.web_search_agent import search_topic
-
+from agents.exam_generator import generate_exam
 
 def create_learning_plan(topic,days):
 
     roadmap = generate_course_plan(topic , days)
 
     resources = search_topic(topic)
+
+    exam = generate_exam(topic)
     
     return{
         "roadmap":roadmap,
-        "resources":resources
-
+        "resources":resources,
+        "exam": exam
     }
