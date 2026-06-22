f0c7b485d8f040583547d94d4c23f1e77e124057
nehasirohi07 <nehasirohi665@gmail.com>
2026-06-22 17:50:03 +0530

    add pydantic response model for course endpoint

--- schemas/course.py
+++ schemas/course.py
@@ -4,3 +4,9 @@ from pydantic import BaseModel
 class CourseRequest(BaseModel):
     topic : str
     days : int
+
+class CourseResponse(BaseModel):
+    topic : str
+    days : int
+    roadmap : dict
+    resources : list[str]
