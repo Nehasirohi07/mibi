a00edb2426fac40f4d42461cb70e57237acec534
nehasirohi07 <nehasirohi665@gmail.com>
2026-06-22 18:00:53 +0530

    refactor API structure with response models

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI
-from pydantic import BaseModel 
-from agents.course_planner import generate_course_plan
-from agents.web_search_agent import search_topic
+
 from agents.orchestrator import create_learning_plan
+
+from schemas.course import (
+    CourseRequest,
+    CourseResponse
+)
 
 app = FastAPI()
 
-class CourseRequest(BaseModel):
-    topic: str
-    days: int
 
 
 @app.get("/")
@@ -17,7 +17,10 @@ def home():
         "message":"Mibi Backend Running"
     }
 
-@app.post("/course")
+@app.post(
+        "/course",
+        response_model = CourseResponse
+          )
 def create_course(course: CourseRequest):
     
     result = create_learning_plan(
