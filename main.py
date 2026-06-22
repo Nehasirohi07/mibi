42a8a15a18ceda2083ce2aec9157eaf3ffeb84ce
nehasirohi07 <nehasirohi665@gmail.com>
2026-06-22 17:35:35 +0530

    Update

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
-def main():
-    print("Hello from mibi!")
+from fastapi import FastAPI
+from pydantic import BaseModel 
+from agents.course_planner import generate_course_plan
+from agents.web_search_agent import search_topic
+from agents.orchestrator import create_learning_plan
+
+app = FastAPI()
+
+class CourseRequest(BaseModel):
+    topic: str
+    days: int
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def home():
+    return{
+        "message":"Mibi Backend Running"
+    }
+
+@app.post("/course")
+def create_course(course: CourseRequest):
+    
+    result = create_learning_plan(
+        course.topic,
+        course.days
+    )
+
+    return {
+        "topic": course.topic,
+        "days":course.days,
+        **result 
+    }
