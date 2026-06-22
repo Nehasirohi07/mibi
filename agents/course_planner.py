5693a916a525150ac4cbf6d12da5ebcd7acf1e60
nehasirohi07 <nehasirohi665@gmail.com>
2026-06-22 17:34:52 +0530

    Added Course Planner

---
+++ agents/course_planner.py
@@ -0,0 +1,13 @@
+def generate_course_plan(topic, days):
+
+    roadmap = {
+        "day1": f"Introduction to {topic}",
+        "day2":"Basic Concepts",
+        "day3":"Core Fundamentals",
+        "day4":"Practice Problems",
+        "day5":"Intermediate Topic",
+        "day6":"Mini Project",
+        "day7":"Final Project + Revision"
+    }
+
+    return roadmap
